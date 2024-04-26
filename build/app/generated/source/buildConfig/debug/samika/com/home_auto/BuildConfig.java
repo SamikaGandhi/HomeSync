afbc9070d67cf0a4a41c5f8ee68348e85e8414a2
Samika Gandhi <130069182+damnsamm@users.noreply.github.com>
2024-04-26 21:04:12 +0530

    Add files via upload

---
+++ build/app/generated/source/buildConfig/debug/samika/com/home_auto/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package samika.com.home_auto;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "samika.com.home_auto";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
